2155620dad5a02a287bd4997b3daf126953ab710
NelsonDJCR <dev@nelsondjcr.com>
2023-07-21 16:45:22 -0500

    create: helpers

---
+++ app/helpers.php
@@ -0,0 +1,7 @@
+<?php
+
+// Get Token for https://test.conectadosweb.com.co/
+function getToken(): string
+{
+    return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9eyJ0ZXN0IjozMjE0MTIsInVzZXIiOiJmM3IyIn0NcPLPRLSvfszQwtxZLyypsm3Y56ELRdppqYXDv2Hagk";
+}
