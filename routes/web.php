026dd7c582043d2c8ac52f58a9031df20737827e
NelsonDJCR <dev@nelsondjcr.com>
2023-07-21 19:55:59 -0500

    register: routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ApiController;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::controller(ApiController::class)->group(function () {
+    Route::get('user/{id}', 'getUser');
+    Route::get('users', 'getUsers');
+});
+
