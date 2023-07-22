6b1375a590faed81a94e56ee95a20cb3c4f75ab0
NelsonDJCR <dev@nelsondjcr.com>
2023-07-21 19:55:08 -0500

    build: api controller

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,29 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Contracts\View\View;
 use Illuminate\Support\Facades\Http;
 
-use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
-    public function fetchApiData()
+    public function getUser($id): View
     {
-        $apiKey = config('app.api_key');
-        $response = Http::get("https://api.example.com/data", [
-            'api_key' => $apiKey,
-            // Add any other required query parameters
-        ]);
+        $user = $this->fetchGetCharacters($id);
+        return view('user', compact('user'));
+    }
 
-        if ($response->successful()) {
-            // API call was successful
-            $data = $response->json(); // Convert the response to a JSON object or array
-            // Process the data as needed
-        } else {
-            // Handle the error
-            $errorCode = $response->status();
-            $errorMessage = $response->json(); // If the error response is in JSON format
-            // Handle the error accordingly
-        }
+    function getUsers(): View
+    {
+        $page =  request()->get('page') ?? 1;
+        $table = $this->fetchGetCharacter($page);
+
+        return view('users', compact('table'));
+    }
+
+    function fetchGetCharacters($id): Array
+    {
+        $user = Http::get("https://reqres.in/api/users/$id");
+        return $user->json();
+    }
+    function fetchGetCharacter($page): Array
+    {
+        $table = Http::get("https://reqres.in/api/users?page=$page");
+        return $table->json();
     }
 }
